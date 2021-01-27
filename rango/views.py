009b19b315e7fd636b42cfcb5ce50e8646bc1d17
Yogesh Parajuli <yogeshparajuli56@gmail.com>
2021-01-27 01:25:49 +0000

    4.2 Completed

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    # Construct a dictionary to pass to the template engines as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client (instead of an HttpResponse
+    # We make use fo teh shortcut function to make out lives easier.
+    # note that the first parameter is teh template we wish to use
+    return render(request, 'rango/index.html', context=context_dict)
 
 # View from the exercise
 def about(request):
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# Pointing to the directory where static files are stored.
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Users/yogeshparajuli/Documents/WAD/Workspace/tango_with_django_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Data structure that Django can parse to work out where our new directory is
+STATICFILES_DIRS = [STATIC_DIR, ]
